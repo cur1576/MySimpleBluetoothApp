8b0a0a59d2e9a43da44a794453f21ec9e04165da
cur1576 <cur1576@gmx.de>
2019-03-11 12:03:13 +0100

    commit_11.03._06

---
+++ app/src/main/java/com/example/mysimplebluetoothapp/ConnectedDevices.java
@@ -0,0 +1,23 @@
+package com.example.mysimplebluetoothapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ConnectedDevices extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connected_devices);
+    }
+
+    public void connAsServer(View view) {
+    }
+
+    public void cannAsClient(View view) {
+    }
+
+    public void sendMessage(View view) {
+    }
+}
