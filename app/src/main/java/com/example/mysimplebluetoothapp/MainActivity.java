bd83af7996b42845d55d27b7f89df54750eba854
cur1576 <cur1576@gmx.de>
2019-03-11 09:19:22 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mysimplebluetoothapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.mysimplebluetoothapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void turnOn(View view) {
+    }
+
+    public void turnOff(View view) {
+    }
+
+    public void getVisible(View view) {
+    }
+
+    public void listDevices(View view) {
+    }
+
+    public void searchDevices(View view) {
+    }
+}
